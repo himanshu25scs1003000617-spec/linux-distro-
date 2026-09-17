1658ca6ce3d3d5ec392b672419a69b89c886ea86
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2026-09-18 03:01:50 +0530

    fix(archiso): clean file_permissions array and add memtest86+-efi

--- profiledef.sh
+++ profiledef.sh
@@ -15,11 +15,6 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/etc/gshadow"]="0:0:400"
-  ["/etc/sudoers.d"]="0:0:750"
   ["/etc/sudoers.d/00-liveuser"]="0:0:440"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/live-setup.sh"]="0:0:755"
 )
